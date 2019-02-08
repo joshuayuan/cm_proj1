ec72c7f7334c050111d0ac5a8077df4734327ca1
Joshua Yuan <joshua43210@gmail.com>
2019-02-07 17:26:43 -0800

    add audio clip

--- pi-client/client.js
+++ pi-client/client.js
@@ -1,5 +1,7 @@
 var io = require('socket.io-client');
 var socket = io('http://joshuayuan.me:8081');
+var player = require('play-sound')(opts = {});
+
 
 socket.on('connect', function() {
   console.log("client connecting");
@@ -11,5 +13,8 @@ socket.on('connect', function() {
 socket.on('server:pi', function(data, response) {
   console.log("PI receive from server:");
   console.log(data);
+  player.play('audio/audio1.mp3', {play: ''},  function(err) {
+    if (err) throw err;
+  });
 });
 
